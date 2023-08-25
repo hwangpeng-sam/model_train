d601219c46bf3880b304a391e0b526aa80da63b2
seokyang <uoon97@gmail.com>
2023-08-25 13:42:41 +0900

    modify upload file code

--- 03.upload_model.sh
+++ 03.upload_model.sh
@@ -1,4 +1,5 @@
 chmod 777 $(pwd)
 docker build -t train:0.0 .
 docker run -v $(pwd):/train train:0.0
-aws s3 sync s3://bucket-plugissue/model /home/ec2-user/model_train
+# model.pt 이름 수정 필요.
+aws s3 cp model.pt s3://bucket-plugissue/model
--- 04.upload_s3.sh
+++
@@ -1,2 +0,0 @@
-# model.pt 이름 수정 필요.
-aws s3 sync model.pt s3://bucket-plugissue/model
