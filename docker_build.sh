86e94a99294a4360a0f77c6eff8815051f1fd6fe
seokyang <uoon97@gmail.com>
2023-08-25 12:50:07 +0900

    test sync s3 & container

--- docker_build.sh
+++ docker_build.sh
@@ -1,4 +1,4 @@
-chmod +x 
+chmod 777 $(pwd)
 docker build -t train:0.0 .
 docker run -v $(pwd):/train train:0.0
-aws s3 sync s3://plugissue/model /home/ec2-user/model_train
+aws s3 sync s3://bucket-plugissue/model /home/ec2-user/model_train
